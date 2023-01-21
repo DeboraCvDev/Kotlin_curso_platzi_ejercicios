1ce8d387d3fda342750b622391537618172434e7
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-21 12:23:32 -0500

    ejercicio_uno_variables_kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+const val PI = 3.1416
 fun main(args: Array<String>) {
     println("Hola")
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+val nombre = "María"
+    println(nombre)
+println(PI)
 }
