67a9529f18f6ad8d4dec4165f9fe91ddb7b92bf0
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-21 14:16:02 -0500

    ejercicio_cinco_bucles_while_y_do_while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
     println("Hola")
+    var contador= 10
+    while (contador > 0){
+        println("El valor de contador es $contador")
+        contador--
+    }
+    do {
+        println("Gernerando numero aleatorio")
+        val numeroAleatorio = (0..100).random()
+        println("El numero generado es $numeroAleatorio")
+    }while (numeroAleatorio > 50)
 }
