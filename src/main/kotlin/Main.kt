45a356993a35cc4d95bd76a18e9e9fa0f113ea32
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-21 14:41:14 -0500

    ejercicio_seis_funciones_y_funciones_de_extension

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
     println("Hola")
+    val listaDeFrutas: List<String> = listOf("Manzana", "Pera", "frambuesa", "Durazno")
+    for (fruta in listaDeFrutas) println("Hoy voy a comemerme una fruta llamada $fruta")
+    listaDeFrutas.forEach { fruta -> println("Hoy voy a comemerme una fruta nueva se llama $fruta") }
+
+    val caracteresDeFruta: List<Int> = listaDeFrutas .map { fruta -> fruta.length }
+    println(caracteresDeFruta)
+
+    val listaFiltrada = caracteresDeFruta.filter { largoDeFruta -> largoDeFruta > 5 }
+    println(listaFiltrada)
 }
