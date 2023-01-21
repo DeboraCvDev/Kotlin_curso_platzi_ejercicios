5b5d552bca41626277be22859dfd2a180a4eb37f
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-21 12:49:33 -0500

    ejercicio_dos_kotlin_y_sus_tipos_de_variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hola")
+  val boolean = true
+    val numeroLargo = 3L
+    val double : Double = 2.7182
+    val float = 1.1f
+
+
+    val primerValor = 20
+    val segundoValor = 10
+    val tercerValor = primerValor - segundoValor
+    println(tercerValor)
+
+
+    val apellido = "Vetri"
+    val giuseppe = "Giuseppe"
+    val nombreCompleto = "Mi nombre es $giuseppe $apellido"
+    println(nombreCompleto)
 }
