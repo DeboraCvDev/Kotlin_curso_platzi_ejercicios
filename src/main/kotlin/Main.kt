ecd0aadfdcd811544a6b712b0c238936d527156e
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-21 13:36:00 -0500

    ejercicio_tres_estructuras_de_control_if

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
     println("Hola")
+    val nombre = "li"
+
+    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else
+        println("Error , la variable esta vacia")
+
+    val mensaje : String = if (nombre.length > 4) {
+        "Tu nombre es largo!"
+    }else if(nombre .isEmpty()){
+        "Nombre esta vacio"
+    } else{
+        "Tienes un nombre corto"
+    }
+    println(mensaje)
 }
