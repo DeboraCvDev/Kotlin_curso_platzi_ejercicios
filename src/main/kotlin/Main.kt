99dabc59bb11b37e93058386b6432c831d1300e8
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-23 14:30:37 -0500

    ejercicio_ocho_lambdas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
     println("Hola")
+    val myLambda : (String)->Int={ valor -> valor.length}
+    val lambdaEjecutada: Int = myLambda("Hola platzi")
+    println(lambdaEjecutada)
+
+    val saludos = listOf("Hello","hola","ciao")
+    val longitudDeSaludos = saludos.map(myLambda)
+    println(longitudDeSaludos)
 }
