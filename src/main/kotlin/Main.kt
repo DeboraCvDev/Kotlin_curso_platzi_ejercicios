49b4f4ea22389f3cdebec7ced646a81ea6287c9d
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-25 11:15:42 -0500

    ejercicio_nueve_high_order_functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
     println("Hola")
+    val largoDelValorInicial = superFuncion(valorInicial = "Hola!") { valor ->
+        valor.length
+    }
+    println(largoDelValorInicial)
+
+    val lambda = funcionInception(nombre = "Enrique")
+    val valorLambda: String = lambda()
+    println(valorLambda)
+}
+
+fun superFuncion(valorInicial : String, block : (String) -> Int) : Int {
+    return block(valorInicial)
+}
+
+fun funcionInception(nombre: String) : () -> String {
+    return {
+        "Hola desde la lambda $nombre"
+    }
 }
