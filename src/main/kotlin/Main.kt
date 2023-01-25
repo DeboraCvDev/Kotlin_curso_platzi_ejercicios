7bc329d9180a5b9a5f8ebabd131a380ad266740d
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-25 11:38:46 -0500

    ejercicio_once_with

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
     println("Hola")
+    val colores = listOf("Azul","Amarillo","Rojo")
+    with(colores){
+        println("Nuestros colores son $this")
+        println("Esta lista tiene una cantidad de colores de $size")
+    }
 }
