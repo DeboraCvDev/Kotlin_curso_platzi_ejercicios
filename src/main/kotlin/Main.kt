623b887e08000cd6a74fc7ed1bf3b709559387d4
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-25 11:33:17 -0500

    ejercicio_diez_let

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
     println("Hola")
+    var nombre : String? = null
+    nombre?.let {
+        valor -> println("El nombre es nulo, es $valor")
+    }
+    nombre = "Giuseppe"
+    nombre?.let{
+    valor -> println("El nombre no es nulo, es $valor")
+}
 }
