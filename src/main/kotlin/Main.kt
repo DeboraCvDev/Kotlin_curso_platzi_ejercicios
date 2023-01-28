3724f32c21cfe4af0bb0418987188d9afe099a86
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-28 14:24:44 -0500

    ejercicio_trece_apply

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-    println("Hola")
+    val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei Redmi 9", "Xiaomi mi a3")
+        .run {
+            removeIf { movil -> movil.contains("Google") }
+            this
+        }
+    println(moviles)
+
+    val colores : MutableList<String>? = mutableListOf("Amarillo","Azul","Rojo")
+    colores?.apply {
+        println("Nuestros colores son $this")
+        println("la cantidad de colores es $size")
+    }
 }
